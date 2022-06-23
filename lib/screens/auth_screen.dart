3ede954e8fa06f0171f925057f4dcc13c341f5a3
Ay-shi <soodayushi18.as@gmail.com>
2022-06-23 22:11:52 +0530

    animated container

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -209,100 +209,103 @@ class _AuthCardState extends State<AuthCard>
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
     return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      elevation: 8.0,
-      child: AnimatedBuilder(
-        animation: _heightAnimation,
-        builder: (ctx, ch) => Container(
-            //height: _authMode == AuthMode.Signup ? 320 : 260,
-            height: _heightAnimation.value.height,
-            constraints:
-                BoxConstraints(minHeight: _heightAnimation.value.height),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        elevation: 8.0,
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 300),
+          child: Container(
+            height: _authMode == AuthMode.Signup ? 320 : 260,
+            //height: _heightAnimation.value.height,
+            constraints: BoxConstraints(
+              //minHeight: _heightAnimation.value.height,
+              minHeight: _authMode == AuthMode.Signup ? 320 : 260,
+            ),
             width: deviceSize.width * 0.75,
             padding: EdgeInsets.all(16.0),
-            child: ch),
-        child: Form(
-          key: _formKey,
-          child: SingleChildScrollView(
-              child: Column(children: <Widget>[
-            TextFormField(
-              decoration: InputDecoration(labelText: 'E-Mail'),
-              keyboardType: TextInputType.emailAddress,
-              validator: (value) {
-                if (value == null || !value.contains('@')) {
-                  return 'Invalid email!';
-                }
-                return null;
-              },
-              onSaved: (value) {
-                _authData['email'] = value!;
-              },
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-              controller: _passwordController,
-              validator: (value) {
-                if (value == null || value.length < 5) {
-                  return 'Password is too short!';
-                }
-              },
-              onSaved: (value) {
-                _authData['password'] = value!;
-              },
-            ),
-            if (_authMode == AuthMode.Signup)
-              TextFormField(
-                enabled: _authMode == AuthMode.Signup,
-                decoration: InputDecoration(labelText: 'Confirm Password'),
-                obscureText: true,
-                validator: _authMode == AuthMode.Signup
-                    ? (value) {
-                        if (value != _passwordController.text) {
-                          return 'Passwords do not match!';
-                        }
-                      }
-                    : null,
-              ),
-            SizedBox(
-              height: 20,
-            ),
-            if (_isLoading)
-              CircularProgressIndicator()
-            else
-              RaisedButton(
-                child: Text(_authMode == AuthMode.Login ? 'LOGIN' : 'SIGN UP'),
-                onPressed: _submit,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
+            child: Form(
+              key: _formKey,
+              child: SingleChildScrollView(
+                  child: Column(children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'E-Mail'),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value == null || !value.contains('@')) {
+                      return 'Invalid email!';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _authData['email'] = value!;
+                  },
                 ),
-                padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
-                color: Theme.of(context).primaryColor,
-                textColor: Theme.of(context).colorScheme.tertiary,
-              ),
-            FlatButton(
-              child: Text(
-                  '${_authMode == AuthMode.Login ? 'SIGNUP' : 'LOGIN'} INSTEAD'),
-              onPressed: _switchAuthMode,
-              padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
-              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-              textColor: Theme.of(context).primaryColor,
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Password'),
+                  obscureText: true,
+                  controller: _passwordController,
+                  validator: (value) {
+                    if (value == null || value.length < 5) {
+                      return 'Password is too short!';
+                    }
+                  },
+                  onSaved: (value) {
+                    _authData['password'] = value!;
+                  },
+                ),
+                if (_authMode == AuthMode.Signup)
+                  TextFormField(
+                    enabled: _authMode == AuthMode.Signup,
+                    decoration: InputDecoration(labelText: 'Confirm Password'),
+                    obscureText: true,
+                    validator: _authMode == AuthMode.Signup
+                        ? (value) {
+                            if (value != _passwordController.text) {
+                              return 'Passwords do not match!';
+                            }
+                          }
+                        : null,
+                  ),
+                SizedBox(
+                  height: 20,
+                ),
+                if (_isLoading)
+                  CircularProgressIndicator()
+                else
+                  RaisedButton(
+                    child:
+                        Text(_authMode == AuthMode.Login ? 'LOGIN' : 'SIGN UP'),
+                    onPressed: _submit,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
+                    color: Theme.of(context).primaryColor,
+                    textColor: Theme.of(context).colorScheme.tertiary,
+                  ),
+                FlatButton(
+                  child: Text(
+                      '${_authMode == AuthMode.Login ? 'SIGNUP' : 'LOGIN'} INSTEAD'),
+                  onPressed: _switchAuthMode,
+                  padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
+                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  textColor: Theme.of(context).primaryColor,
+                ),
+              ])
+                  // Container(
+                  //   //height: _authMode == AuthMode.Signup ? 320 : 260,
+                  //   height: _heightAnimation.value.height,
+                  //   constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
+                  //   width: deviceSize.width * 0.75,
+                  //   padding: EdgeInsets.all(16.0),
+                  //   child:
+                  //         ],
+                  //       ),
+                  ),
             ),
-          ])
-              // Container(
-              //   //height: _authMode == AuthMode.Signup ? 320 : 260,
-              //   height: _heightAnimation.value.height,
-              //   constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
-              //   width: deviceSize.width * 0.75,
-              //   padding: EdgeInsets.all(16.0),
-              //   child:
-              //         ],
-              //       ),
-              ),
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
